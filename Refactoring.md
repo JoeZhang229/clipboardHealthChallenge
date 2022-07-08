b3318e38d2830a46f14dbdd682730420f367b9d7
Joe <Joe.zhang229@gmail.com>
2022-07-07 19:16:23 -0700

    refactor breakdown comments

--- Refactoring.md
+++ Refactoring.md
@@ -9,3 +9,4 @@ You've been asked to refactor the function `deterministicPartitionKey` in [`dpk.
 You will be graded on the exhaustiveness and quality of your unit tests, the depth of your refactor, and the level of insight into your thought process provided by the written explanation.
 
 ## Your Explanation Here
+    Tests need for this functionality would be for long string length input, nonString inputs. Non-Objects would also need to be tested since key-value is being checked in 'event.partitionKey.'
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,23 @@
-const { deterministicPartitionKey } = require("./dpk");
+const crypto = require('crypto');
+const { deterministicPartitionKey } = require('./dpk');
 
-describe("deterministicPartitionKey", () => {
-  it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
-  });
+describe('deterministicPartitionKey', () => {
+	it("Returns the literal '0' when given no input", () => {
+		const trivialKey = deterministicPartitionKey();
+		expect(trivialKey).toBe('0');
+	});
+});
+
+describe('PartitionKeyLength', () => {
+	it('Returns a hash when key length is longer than 256 characters', () => {
+		const longString = { partitionKey: 'longString'.repeat(20 * 20) };
+		// expect(deterministicPartitionKey(longString)).toBe();
+	});
+});
+
+describe('NotString', () => {
+	it('Converts and returns a string when input is a number', () => {
+		const numInput = deterministicPartitionKey(5);
+		expect(typeof numInput).toBe('string');
+	});
 });
